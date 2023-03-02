eec768e8c4f4e484c5bd0c0f45804c25e15d6877
quangminh <quangminh@gmail.com>
2023-03-02 21:03:02 +0700

    br04-post-view

--- client/src/pages/post/PostViewId.tsx
+++ client/src/pages/post/PostViewId.tsx
@@ -1,9 +1,114 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
+import { getAxiosData } from "../../axios/axiosConfig";
+import { selectAuth } from "../../features/auth/authSlice";
 
 type Props = {};
 
 const PostViewId = (props: Props) => {
-  return <div>PostViewId</div>;
+  let navigate = useNavigate();
+  const { id } = useParams();
+  const { accessToken, user } = useAppSelector(selectAuth);
+  const dispatch = useAppDispatch();
+  const [postObject, setPostObject] = useState<any>({});
+  const [newComment, setNewComment] = useState("");
+  useEffect(() => {
+    if (id) {
+      (async () => {
+        const resData = await getAxiosData(
+          `/api/posts/getPostById/${id}`,
+          accessToken,
+          dispatch
+        );
+        if (resData) {
+          setPostObject(resData.data.post);
+        }
+      })();
+    }
+  }, [accessToken, dispatch, id]);
+
+  const addComment = () => {
+    console.log(newComment)
+  };
+
+  // const deleteComment = (id: number) => {};
+
+  const deletePost = (id: number) => {
+    console.log(id);
+  };
+  const updatePost = (id: number) => {
+    navigate(`/posts/update/${id}`);
+  };
+
+  return (
+    postObject && (
+      <div className="postPage">
+        <div className="leftSide">
+          <div className="post" id="individual">
+            <div className="title">{postObject.title}</div>
+            <div className="body">{postObject.postText}</div>
+            <div className="footer">
+              {postObject.username}
+              {user.username === postObject.username && (
+                <button
+                  onClick={() => {
+                    deletePost(postObject.id);
+                  }}
+                >
+                  {" "}
+                  Delete
+                </button>
+              )}
+              {user.username === postObject.username && (
+                <button
+                  onClick={() => {
+                    updatePost(postObject.id);
+                  }}
+                >
+                  {" "}
+                  Update
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="rightSide">
+          <div className="addCommentContainer">
+            <input
+              type="text"
+              placeholder="Comment..."
+              autoComplete="off"
+              value={newComment}
+              onChange={(event) => {
+                setNewComment(event.target.value);
+              }}
+            />
+            <button onClick={addComment}> Add Comment</button>
+          </div>
+          <div className="listOfComments">
+            {/* {comments.map((comment, key) => {
+              return (
+                <div key={key} className="comment">
+                  {comment.commentBody}
+                  <label> Username: {comment.username}</label>
+                  {authState.username === comment.username && (
+                    <button
+                      onClick={() => {
+                        deleteComment(comment.id);
+                      }}
+                    >
+                      X
+                    </button>
+                  )}
+                </div>
+              );
+            })} */}
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default PostViewId;
--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -1,12 +1,15 @@
 import express from "express";
 
 import { validateToken } from "../middlewares/auth";
-import { createPost, getPosts } from "../controllers/post";
+import { createPost, getPostById, getPosts } from "../controllers/post";
 
 const router = express.Router();
 
 //  get posts
 router.get("/", validateToken, getPosts);
+
+// get post by id
+router.get("/getPostById/:id", validateToken, getPostById);
 
 // create post
 router.post("/create", validateToken, createPost);
